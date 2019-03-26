e434b7a65007cfd73e8b4e04ce19afd734016bce
siyong.zhu <siyong.zhu@irdeto.com>
2019-03-25 23:46:54 -0400

    first commit

---
+++ src/main/java/com/notebook/demo/NoteBookPackApplication.java
@@ -0,0 +1,13 @@
+package com.notebook.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBookPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteBookPackApplication.class, args);
+	}
+
+}
